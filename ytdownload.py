300dc92c8e8ac3ad835e817087f876cd06c67790
Yashwanth1026 <152046071+Yashwanth1026@users.noreply.github.com>
2024-09-16 12:34:32 +0530

    Update ytdownload.py

--- ytdownload.py
+++ ytdownload.py
@@ -3,19 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
-# Create the FastAPI app
 app = FastAPI()
 
-# Add CORS middleware to allow requests from any origin (you can limit it to specific origins for security)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins, can be restricted
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# Define a Pydantic model for the request body
 class VideoURL(BaseModel):
     url: str
 
@@ -29,7 +25,7 @@ def download_youtube_video(video: VideoURL):
    
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([video.url])  # Use the URL from the request body
+            ydl.download([video.url])  
         return {"status": "success", "message": "Download complete!"}
     except Exception as e:
         return {"status": "error", "message": str(e)}
